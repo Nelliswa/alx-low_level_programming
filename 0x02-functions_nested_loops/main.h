b76e7ff56d7264cdf0367ebefc9fa08eab1eb118
Neliswa <shingathobeka38@gmail.com>
2023-06-23 10:43:42 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int putchar(char c);
-void print_alphabet(void);
-void print_alphabet(void);
+int _putchar(char c);
+void print_alphabet(void)
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -16,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#ifndef
