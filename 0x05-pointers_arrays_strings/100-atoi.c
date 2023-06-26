9933ed9f50b7da0dd4c0d42785b334a8a930fb49
Neliswa <shingathobeka38@gmail.com>
2023-06-26 11:24:08 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,22 +3,20 @@
 
 /**
 
- * _atoi - converts a string to an integer
+* _atoi - converts a string to an integer
 
- * @s: string to be converted
+* @s: string to be converted
 
- *
+*
 
- * Return: the int converted from the string
+* Return: the int converted from the string
 
- */
+*/
 
 int _atoi(char *s)
 
 {
-
-        int i, d, n, len, f, digit;
-
+	int i, d, n, len, f, digit;
 
         i = 0;
 
